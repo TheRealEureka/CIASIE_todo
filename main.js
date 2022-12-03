358de1c204e2a01f0e698d9df01c40776fc804bf
Eurêka <therealeureka@protonmail.com>
2022-12-03 09:51:53 +0100

    Add tabs

--- main.js
+++ main.js
@@ -9,26 +9,29 @@ let myApp = {
     data() {
         return {
             task : localStorage.getItem('todo') !== null ? JSON.parse(localStorage.getItem('todo')) : [],
-            newtask : {name:'', date : '', done : false, edit : false},
-
+            list : localStorage.getItem('lists') !== null ? JSON.parse(localStorage.getItem('lists')) : [],
+            newlist : {name:'', description:'', edit:false},
+            newtask : {name:'', date : '', done : false, edit : false, listid : -1},
+            current_list : -1,
+            showpopup: false,
         }
     },
     computed: {
-        undone() {
-            return this.task.filter(function(item, index) {
-                item.id = index;
-                return !item.done
-            });
-        },
-        done() {
-            return this.task.filter(function(item, index) {
-                item.id = index;
-                return item.done
-            });
-        }
 
     },
     methods: {
+        undone(listid) {
+            return this.task.filter(function(item, index) {
+                item.id = index;
+                return !item.done && item.listid === listid;
+            });
+        },
+        done(listid) {
+            return this.task.filter(function(item, index) {
+                item.id = index;
+                return item.done && item.listid === listid;
+            });
+        },
         deleteExp(index) {
             this.task.splice(index,1);
             this.save();
@@ -37,9 +40,11 @@ let myApp = {
         save(){
 
             localStorage.setItem('todo', JSON.stringify(this.task));
+            localStorage.setItem('lists', JSON.stringify(this.list));
         },
         addExp(){
             if(this.newtask.name !== '' && this.newtask.date !== '') {
+                this.newtask.listid = this.current_list;
                 this.task.push(this.newtask);
                 this.newtask = {name:'', date : '', done : false, edit:false};
                 this.save();
@@ -54,6 +59,48 @@ let myApp = {
             }else{
                 this.task[index].edit = true;
             }
+        },
+        checkDate(date){
+            let today = new Date();
+            let taskDate = new Date(date);
+            return today > taskDate;
+        },
+        startDrag(evt, item) {
+            console.log('startDrag', item)
+            evt.dataTransfer.dropEffect = 'move'
+            evt.dataTransfer.effectAllowed = 'move'
+            evt.dataTransfer.setData('itemID', item.id)
+        },
+        onDrop(evt, list) {
+            this.task[evt.dataTransfer.getData('itemID')].listid = list;
+            this.save();
+        },
+        addList(){
+            if(this.newlist.name !== '') {
+                this.list.push(this.newlist);
+                this.newlist = {name:'', description:'', edit:false};
+                this.save();
+            }
+        },
+        editList(index){
+            if(this.list[index].edit === true) {
+                if(this.list.name !== '') {
+                    this.list[index].edit = false;
+                    this.save();
+                }
+            }else{
+                this.list[index].edit = true;
+            }
+        },
+        deleteList(index) {
+            this.list.splice(index,1);
+            for(let i = 0; i < this.task.length; i++){
+                if(this.task[i].listid === index){
+                    this.task.splice(i,1);
+                    i--;
+                }
+            }
+            this.save();
         }
 
     }
